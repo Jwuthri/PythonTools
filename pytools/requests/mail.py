d1229f5e59345b27f78db919822c7d4bc941eca6
Wuthrich Julien <julien.wut@gmail.com>
2017-05-15 15:42:28 +0200

    Update mail.py

--- pytools/requests/mail.py
+++ pytools/requests/mail.py
@@ -8,13 +8,13 @@ import email
 import smtplib
 
 
-def send_mail(sender, receiver, subject, message, attach_file=None):
+def send_mail(sender, receiver, subject, message, server, attach_file=None):
     msg = email.mime.multipart.MIMEMultipart()
     msg["From"] = str(object=sender)
     msg["To"] = str(object=receiver)
     msg["Subject"] = str(object=subject)
     msg.attach(email.mime.text.MIMEText(message))
-    mailserver = smtplib.SMTP("Julien.Wut@gmail.com", 25)
+    mailserver = smtplib.SMTP(server, 25)
     if attach_file:
         part = email.mime.base.MIMEBase('application', "octet-stream")
         part.set_payload(open(attach_file, "rb").read())
